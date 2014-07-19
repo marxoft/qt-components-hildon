b8d5862d52f199c0e3ba465519ca5ff9a8bfeb36
Stuart Howarth <showarth@marxoft.co.uk>
2014-07-19 20:52:03 +0100

    Add WebHistory and WebHistoryInterface

---
+++ src/webkit/webhistory_p.h
@@ -0,0 +1,90 @@
+/*
+ * Copyright (C) 2014 Stuart Howarth <showarth@marxoft.co.uk>
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU Lesser General Public License,
+ * version 3, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St - Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#ifndef WEBHISTORY_P_H
+#define WEBHISTORY_P_H
+
+#include <QObject>
+#include <qdeclarative.h>
+
+class QWebHistory;
+
+class WebHistory : public QObject
+{
+    Q_OBJECT
+
+    Q_PROPERTY(QVariantList backItems READ backItems CONSTANT)
+    Q_PROPERTY(QVariantList forwardItems READ forwardItems CONSTANT)
+    Q_PROPERTY(QVariantList items READ items CONSTANT)
+    Q_PROPERTY(QVariant backItem READ backItem NOTIFY currentIndexChanged)
+    Q_PROPERTY(QVariant forwardItem READ forwardItem NOTIFY currentIndexChanged)
+    Q_PROPERTY(QVariant currentItem READ currentItem NOTIFY currentIndexChanged)
+    Q_PROPERTY(int currentIndex READ currentIndex WRITE setCurrentIndex NOTIFY currentIndexChanged)
+    Q_PROPERTY(int maximumItemCount READ maximumItemCount WRITE setMaximumItemCount NOTIFY maximumItemCountChanged)
+    Q_PROPERTY(int count READ count CONSTANT)
+    Q_PROPERTY(bool canGoBack READ canGoBack NOTIFY currentIndexChanged)
+    Q_PROPERTY(bool canGoForward READ canGoForward NOTIFY currentIndexChanged)
+    Q_PROPERTY(QString storageFileName READ storageFileName WRITE setStorageFileName NOTIFY storageFileNameChanged)
+
+public:
+    explicit WebHistory(QWebHistory *history, QObject *parent = 0);
+    ~WebHistory();
+
+    QVariantList backItems() const;
+    QVariantList forwardItems() const;
+    QVariantList items() const;
+
+    QVariant backItem() const;
+    QVariant forwardItem() const;
+    QVariant currentItem() const;
+
+    Q_INVOKABLE QVariant itemAt(int index) const;
+
+    int currentIndex() const;
+    void setCurrentIndex(int index);
+
+    int maximumItemCount() const;
+    void setMaximumItemCount(int count);
+
+    int count() const;
+
+    bool canGoBack() const;
+    bool canGoForward() const;
+
+    QString storageFileName() const;
+    void setStorageFileName(const QString &fileName);
+
+public slots:
+    bool save();
+    bool load();
+
+signals:
+    void currentIndexChanged();
+    void maximumItemCountChanged();
+    void storageFileNameChanged();
+
+private:
+    QWebHistory *m_history;
+
+    QString m_fileName;
+
+    Q_DISABLE_COPY(WebHistory)
+};
+
+QML_DECLARE_TYPE(WebHistory)
+
+#endif // WEBHISTORY_P_H
---
+++ src/webkit/webhistoryinterface.cpp
@@ -0,0 +1,84 @@
+/*
+ * Copyright (C) 2014 Stuart Howarth <showarth@marxoft.co.uk>
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU Lesser General Public License,
+ * version 3, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St - Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#include "webhistoryinterface_p.h"
+#include <QFile>
+#include <QDeclarativeInfo>
+
+WebHistoryInterface::WebHistoryInterface(QObject *parent) :
+    QWebHistoryInterface(parent)
+{
+    QWebHistoryInterface::setDefaultInterface(this);
+}
+
+WebHistoryInterface::~WebHistoryInterface() {}
+
+void WebHistoryInterface::addHistoryEntry(const QString &url) {
+    m_urls.removeOne(url);
+    m_urls << url;
+    emit urlsChanged();
+}
+
+bool WebHistoryInterface::historyContains(const QString &url) const {
+    return m_urls.contains(url);
+}
+
+QStringList WebHistoryInterface::urls() const {
+    return m_urls;
+}
+
+QString WebHistoryInterface::storageFileName() const {
+    return m_fileName;
+}
+
+void WebHistoryInterface::setStorageFileName(const QString &fileName) {
+    if (fileName != this->storageFileName()) {
+        m_fileName = fileName;
+        emit storageFileNameChanged();
+    }
+}
+
+bool WebHistoryInterface::save() {
+    QFile file(this->storageFileName());
+
+    if (file.open(QIODevice::WriteOnly | QIODevice::Text)) {
+        for (int i = 0; i < m_urls.size(); i++) {
+            file.write(m_urls.at(i).toUtf8());
+        }
+
+        file.close();
+        return true;
+    }
+
+    qmlInfo(this) << tr("Cannot save web history:") << file.errorString();
+}
+
+bool WebHistoryInterface::load() {
+    QFile file(this->storageFileName());
+
+    if (file.open(QIODevice::ReadOnly | QIODevice::Text)) {
+        while (!file.atEnd()) {
+            m_urls << file.readLine();
+        }
+
+        file.close();
+        return true;
+    }
+
+    qmlInfo(this) << tr("Cannot load web history:") << file.errorString();
+}
+
