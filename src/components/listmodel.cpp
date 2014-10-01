9e528b32f09d6f66c841bead57de85ab81392fbc
Stuart Howarth <showarth@marxoft.co.uk>
2014-10-01 20:53:41 +0100

    Re-factor ListModel.

--- src/components/listmodel.cpp
+++ src/components/listmodel.cpp
@@ -16,6 +16,7 @@
  */
 
 #include "listmodel_p.h"
+#include "listelement_p.h"
 
 class ListModelPrivate
 {
@@ -36,36 +37,53 @@ public:
 
         if (ListModel *model = qobject_cast<ListModel*>(list->object)) {
             model->d_func()->dataList.append(obj);
+            
+            if (ListElement *element = static_cast<ListElement*>(obj)) {
+                model->d_func()->elements.append(element);
+            }
         }
     }
 
     virtual QDeclarativeListProperty<QObject> data() {
         return QDeclarativeListProperty<QObject>(q_func(), 0, ListModelPrivate::data_append);
     }
+    
+    ListElement* createElement(const QVariantMap &properties) {
+        Q_Q(ListModel);
+        
+        ListElement *element = new ListElement(q);
+        QMapIterator<QString, QVariant> iterator(properties);
+        
+        while (iterator.hasNext()) {
+            iterator.next();
+            element->insert(iterator.key(), iterator.value());
+        }
+        
+        return element;
+    }
 
     void setRoleNames() {
-        if (items.isEmpty()) {
+        if (elements.isEmpty()) {
             return;
         }
 
         roleNames.clear();
-
-        QMapIterator<QString, QVariant> iterator(items.first());
+        
+        ListElement *element = elements.first();
         int role = Qt::UserRole + 1;
-
-        while (iterator.hasNext()) {
-            iterator.next();
-            roleNames[role] = iterator.key().toUtf8();
+        
+        foreach (QString key, element->keys()) {
+            roleNames[role] = key.toUtf8();
             role++;
         }
-
+        
         Q_Q(ListModel);
         q->setRoleNames(roleNames);
     }
 
     ListModel *q_ptr;
 
-    QList<QVariantMap> items;
+    QList<ListElement*> elements;
 
     QHash<int, QByteArray> roleNames;
 
@@ -94,115 +112,109 @@ int ListModel::rowCount(const QModelIndex &parent) const {
     Q_UNUSED(parent);
     Q_D(const ListModel);
 
-    return d->items.size();
+    return d->elements.size();
 }
 
 QVariant ListModel::data(const QModelIndex &index, int role) const {
-    Q_D(const ListModel);
+    if (ListElement *element = this->get(index)) {
+        return element->value(this->roleNames().value(role));
+    }
+    
+    return QVariant();
+}
 
-    return this->get(index).value(d->roleNames.value(role));
+QVariant ListModel::modelIndex(int row, int column, const QModelIndex &parent) const {
+    return QVariant::fromValue(this->index(row, column, parent));
 }
 
 QVariant ListModel::property(const QModelIndex &index, const QString &name) const {
-    return this->get(index).value(name);
-}
-
-QVariant ListModel::property(int row, const QString &name) const {
-    return this->get(row).value(name);
+    if (ListElement *element = this->get(index)) {
+        return element->value(name);
+    }
+    
+    return QVariant();
 }
 
 void ListModel::setProperty(const QModelIndex &index, const QString &name, const QVariant &value) {
-    this->setProperty(index.row(), name, value);
-}
-
-void ListModel::setProperty(int row, const QString &name, const QVariant &value) {
-    if ((row >= 0) && (row < this->rowCount())) {
-        Q_D(ListModel);
-        d->items[row].insert(name, value);
-        const QModelIndex index = this->index(row);
+    if (ListElement *element = this->get(index)) {
+        element->insert(name, value);
         emit dataChanged(index, index);
     }
 }
 
-QVariantMap ListModel::get(const QModelIndex &index) const {
-    return this->get(index.row());
-}
-
-QVariantMap ListModel::get(int row) const {
+ListElement* ListModel::get(const QModelIndex &index) const {
+    const int row = index.row();
+    
     if ((row >= 0) && (row < this->rowCount())) {
         Q_D(const ListModel);
-        return d->items.at(row);
+        return d->elements.at(row);
     }
 
-    return QVariantMap();
+    return 0;
 }
 
-void ListModel::set(const QModelIndex &index, const QVariantMap &item) {
-    this->set(index.row(), item);
-}
-
-void ListModel::set(int row, const QVariantMap &item) {
-    if ((row >= 0) && (row < this->rowCount())) {
-        Q_D(ListModel);
-        QMapIterator<QString, QVariant> iterator(item);
+void ListModel::set(const QModelIndex &index, const QVariantMap &properties) {
+    if (ListElement *element = this->get(index)) {
+        QMapIterator<QString, QVariant> iterator(properties);
 
         while (iterator.hasNext()) {
             iterator.next();
-            d->items[row].insert(iterator.key(), iterator.value());
+            element->insert(iterator.key(), iterator.value());
         }
-
-        const QModelIndex index = this->index(row);
+        
         emit dataChanged(index, index);
     }
 }
 
-void ListModel::append(const QVariantMap &item) {
+void ListModel::append(const QVariantMap &properties) {
     Q_D(ListModel);
 
     this->beginInsertRows(QModelIndex(), this->rowCount(), this->rowCount());
-    d->items.append(item);
+    
+    if (ListElement *element = d->createElement(properties)) {
+        d->elements.append(element);
 
-    if (d->items.size() == 1) {
-        d->setRoleNames();
-    }
-
-    this->endInsertRows();
-    emit countChanged();
-}
-
-void ListModel::insert(const QModelIndex &index, const QVariantMap &item) {
-    this->insert(index.row(), item);
-}
-
-void ListModel::insert(int row, const QVariantMap &item) {
-    if ((row >= 0) && (row < this->rowCount())) {
-        Q_D(ListModel);
-        this->beginInsertRows(QModelIndex(), row, row);
-        d->items.insert(row, item);
-
-        if (d->items.size() == 1) {
+        if (d->elements.size() == 1) {
             d->setRoleNames();
         }
 
         this->endInsertRows();
         emit countChanged();
     }
+}
+
+void ListModel::insert(const QModelIndex &index, const QVariantMap &properties) {
+    const int row = index.row();
+    
+    if ((row >= 0) && (row < this->rowCount())) {
+        Q_D(ListModel);
+        
+        if (ListElement *element = d->createElement(properties)) {
+            d->elements.insert(row, element);
+
+            if (d->elements.size() == 1) {
+                d->setRoleNames();
+            }
+
+            this->endInsertRows();
+            emit countChanged();
+        }
+    }
     else {
-        this->append(item);
+        this->append(properties);
     }
 }
 
-void ListModel::move(const QModelIndex &from, const QModelIndex &to, int count) {
-    this->move(from.row(), to.row(), count);
-}
-
-void ListModel::move(int from, int to, int count) {
+void ListModel::move(const QModelIndex &fromIndex, const QModelIndex &toIndex, int count) {
+    const int from = fromIndex.row();
+    const int to = toIndex.row();
+    
     if ((from >= 0) && ((from + count) < this->rowCount()) && (to >= 0)) {
         Q_D(ListModel);
         this->beginMoveRows(QModelIndex(), from, from + count, QModelIndex(), to);
 
         for (int i = 0; i < count; i++) {
-            d->items.move(from, to + i);
+            d->elements.move(from, to + i);
         }
 
         this->endMoveRows();
@@ -210,14 +222,12 @@ void ListModel::move(int from, int to, int count) {
 }
 
 void ListModel::remove(const QModelIndex &index) {
-    this->remove(index.row());
-}
-
-void ListModel::remove(int row) {
+    const int row = index.row();
+    
     if ((row >= 0) && (row < this->rowCount())) {
         Q_D(ListModel);
         this->beginRemoveRows(QModelIndex(), row, row);
-        d->items.removeAt(row);
+        delete d->elements.takeAt(row);
         this->endRemoveRows();
         emit countChanged();
     }
@@ -227,7 +237,8 @@ void ListModel::clear() {
     if (this->rowCount() > 0) {
         Q_D(ListModel);
         this->beginResetModel();
-        d->items.clear();
+        qDeleteAll(d->elements);
+        d->elements.clear();
         this->endResetModel();
         emit countChanged();
     }
@@ -243,3 +254,4 @@ void ListModel::componentComplete() {
 }
 
 #include "moc_listmodel_p.cpp"
+
--- src/components/listmodel_p.h
+++ src/components/listmodel_p.h
@@ -18,10 +18,12 @@
 #ifndef LISTMODEL_P_H
 #define LISTMODEL_P_H
 
+#include "modelindex_p.h"
 #include <QAbstractListModel>
 #include <QDeclarativeParserStatus>
 #include <qdeclarative.h>
 
+class ListElement;
 class ListModelPrivate;
 
 class ListModel : public QAbstractListModel, public QDeclarativeParserStatus
@@ -41,29 +43,24 @@ public:
     virtual int rowCount(const QModelIndex &parent = QModelIndex()) const;
 
     virtual QVariant data(const QModelIndex &index, int role) const;
+    
+    Q_INVOKABLE QVariant modelIndex(int row, int column = 0, const QModelIndex &parent = QModelIndex()) const;
 
     Q_INVOKABLE QVariant property(const QModelIndex &index, const QString &name) const;
-    Q_INVOKABLE QVariant property(int row, const QString &name) const;
 
     Q_INVOKABLE void setProperty(const QModelIndex &index, const QString &name, const QVariant &value);
-    Q_INVOKABLE void setProperty(int row, const QString &name, const QVariant &value);
 
-    Q_INVOKABLE QVariantMap get(const QModelIndex &index) const;
-    Q_INVOKABLE QVariantMap get(int row) const;
+    Q_INVOKABLE ListElement* get(const QModelIndex &index) const;
 
-    Q_INVOKABLE void set(const QModelIndex &index, const QVariantMap &item);
-    Q_INVOKABLE void set(int row, const QVariantMap &item);
+    Q_INVOKABLE void set(const QModelIndex &index, const QVariantMap &properties);
 
-    Q_INVOKABLE void append(const QVariantMap &item);
+    Q_INVOKABLE void append(const QVariantMap &properties);
 
-    Q_INVOKABLE void insert(const QModelIndex &index, const QVariantMap &item);
-    Q_INVOKABLE void insert(int row, const QVariantMap &item);
+    Q_INVOKABLE void insert(const QModelIndex &index, const QVariantMap &properties);
 
-    Q_INVOKABLE void move(const QModelIndex &from, const QModelIndex &to, int count);
-    Q_INVOKABLE void move(int from, int to, int count);
+    Q_INVOKABLE void move(const QModelIndex &fromIndex, const QModelIndex &toIndex, int count);
 
     Q_INVOKABLE void remove(const QModelIndex &index);
-    Q_INVOKABLE void remove(int row);
 
     Q_INVOKABLE void clear();
 
