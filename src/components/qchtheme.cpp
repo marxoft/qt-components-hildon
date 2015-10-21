9a7876e2737620eb1e0f701d8f4c7fb27442930c
Stuart Howarth <showarth@marxoft.co.uk>
2015-10-21 23:10:16 +0100

    Add QchTheme class.

---
+++ src/components/qchtheme.cpp
@@ -0,0 +1,63 @@
+/*
+ * Copyright (C) 2015 Stuart Howarth <showarth@marxoft.co.uk>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "qchtheme.h"
+#include <QSettings>
+
+static const QString THEME_DESKTOP_FILE("/etc/hildon/theme/index.theme");
+
+/*!
+    \class Theme
+    \brief Provides information about the current theme.
+    
+    \ingroup components
+    
+    \note Theme is a context property and cannot be created in QML.
+    
+    \sa Style
+*/
+QchTheme::QchTheme(QObject *parent) :
+    QObject(parent)
+{
+}
+
+/*!
+    \brief The GTK theme name.
+*/
+QString QchTheme::gtkTheme() const {
+    return QSettings(THEME_DESKTOP_FILE, QSettings::IniFormat).value("X-Hildon-Metatheme/GtkTheme").toString();
+}
+
+/*!
+    \brief The path to the desktop icon.
+*/
+QString QchTheme::icon() const {
+    return QSettings(THEME_DESKTOP_FILE, QSettings::IniFormat).value("Desktop Entry/Icon").toString();
+}
+
+/*!
+    \brief The icon theme name.
+*/
+QString QchTheme::iconTheme() const {
+    return QSettings(THEME_DESKTOP_FILE, QSettings::IniFormat).value("X-Hildon-Metatheme/IconTheme").toString();
+}
+
+/*!
+    \brief The theme display name.
+*/
+QString QchTheme::name() const {
+    return QSettings(THEME_DESKTOP_FILE, QSettings::IniFormat).value("Desktop Entry/Name").toString();
+}
---
+++ src/components/qchtheme.h
@@ -0,0 +1,46 @@
+/*
+ * Copyright (C) 2015 Stuart Howarth <showarth@marxoft.co.uk>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef QCHTHEME_H
+#define QCHTHEME_H
+
+#include <QObject>
+
+class QchTheme : public QObject
+{
+    Q_OBJECT
+    
+    Q_PROPERTY(QString gtkTheme READ gtkTheme NOTIFY changed)
+    Q_PROPERTY(QString icon READ icon NOTIFY changed)
+    Q_PROPERTY(QString iconTheme READ iconTheme NOTIFY changed)
+    Q_PROPERTY(QString name READ name NOTIFY changed)
+
+public:
+    explicit QchTheme(QObject *parent = 0);
+        
+    QString gtkTheme() const;
+    
+    QString icon() const;
+    
+    QString iconTheme() const;
+            
+    QString name() const;
+
+Q_SIGNALS:
+    void changed();
+};
+
+#endif // QCHTHEME_H
