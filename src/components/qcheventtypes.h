27b894771e2b2c44feb73b4df0d0e72ebbebc41e
Stuart Howarth <showarth@marxoft.co.uk>
2017-02-21 04:23:06 +0000

    Add event type contants.

---
+++ src/components/qcheventtypes.h
@@ -0,0 +1,24 @@
+/*
+ * Copyright (C) 2017 Stuart Howarth <showarth@marxoft.co.uk>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef QCHEVENTTYPES_H
+#define QCHEVENTTYPES_H
+
+#include <QEvent>
+
+static const int EVENT_TYPE_THEME_CHANGE = QEvent::registerEventType(1001);
+
+#endif // QCHEVENTTYPES_H
